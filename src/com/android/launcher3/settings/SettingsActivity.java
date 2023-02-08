9a816d4b1c30683521c6f6ec4f435eecc077fb27
Yash Garg <yash.garg.ext@murena.com>
2023-02-08 20:00:19 +0530

    feat(settings): hide option to disable new app icons on homescreen on non drawer

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -22,6 +22,7 @@ import static androidx.preference.PreferenceFragmentCompat.ARG_PREFERENCE_ROOT;
 
 import static com.android.launcher3.BuildConfig.IS_DEBUG_DEVICE;
 import static com.android.launcher3.BuildConfig.IS_STUDIO_BUILD;
+import static com.android.launcher3.SessionCommitReceiver.ADD_ICON_PREFERENCE_KEY;
 import static com.android.launcher3.states.RotationHelper.ALLOW_ROTATION_PREFERENCE_KEY;
 
 import android.app.Activity;
@@ -347,6 +348,9 @@ public class SettingsActivity extends FragmentActivity
                     preference.setDefaultValue(isSingleLayer);
                     ((SwitchPreferenceCompat) preference).setChecked(isSingleLayer);
                     return true;
+
+                case ADD_ICON_PREFERENCE_KEY:
+                    return !MultiModeController.isSingleLayerMode();
             }
 
             return true;
