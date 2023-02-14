9640db0e113869d8fc9e07789cac3ac6effc8d25
Yash Garg <yash.garg.ext@murena.com>
2023-02-15 01:29:36 +0530

    feat(hotseat): copy default config to drawer mode table too

--- src/com/android/launcher3/model/ModelDbController.java
+++ src/com/android/launcher3/model/ModelDbController.java
@@ -28,6 +28,7 @@ import static com.android.launcher3.LauncherSettings.Favorites.addTableToDb;
 import static com.android.launcher3.LauncherSettings.Settings.LAYOUT_DIGEST_KEY;
 import static com.android.launcher3.LauncherSettings.Settings.LAYOUT_DIGEST_LABEL;
 import static com.android.launcher3.LauncherSettings.Settings.LAYOUT_DIGEST_TAG;
+import static com.android.launcher3.provider.LauncherDbUtils.copyTable;
 import static com.android.launcher3.provider.LauncherDbUtils.tableExists;
 
 import android.app.blob.BlobHandle;
@@ -542,6 +543,8 @@ public class ModelDbController {
                     mOpenHelper.loadFavorites(mOpenHelper.getWritableDatabase(),
                             getDefaultLayoutParser(widgetHolder));
                 }
+                copyTable(mOpenHelper.getReadableDatabase(), Favorites.E_TABLE_NAME_ALL,
+                        mOpenHelper.getWritableDatabase(), Favorites.E_TABLE_NAME, mContext);
                 clearFlagEmptyDbCreated();
             } finally {
                 widgetHolder.destroy();
