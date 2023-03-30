b1740dc844d50badcee98813ab26d9e2885501c2
Yash Garg <yash.garg.ext@murena.com>
2023-03-30 15:28:42 +0530

    feat(search): add swipe down search layout and methods

---
+++ bliss/src/foundation/e/bliss/widgets/SwipeSearchContainer.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2025 MURENA SAS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+package foundation.e.bliss.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import foundation.e.bliss.blur.OffsetParent
+
+class SwipeSearchContainer
+@JvmOverloads
+constructor(context: Context, attrs: AttributeSet? = null) :
+    FrameLayout(context, attrs), OffsetParent {
+
+    private val offsetParentDelegate = OffsetParent.OffsetParentDelegate()
+
+    override val offsetX: Float
+        get() = translationX
+
+    override val offsetY: Float
+        get() = translationY
+
+    override val needWallpaperScroll: Boolean
+        get() = true
+
+    override fun setTranslationX(translationX: Float) {
+        super.setTranslationX(translationX)
+        offsetParentDelegate.notifyOffsetChanged()
+    }
+
+    override fun setTranslationY(translationY: Float) {
+        super.setTranslationY(translationY)
+        offsetParentDelegate.notifyOffsetChanged()
+    }
+
+    override fun addOnOffsetChangeListener(listener: OffsetParent.OnOffsetChangeListener) {
+        offsetParentDelegate.addOnOffsetChangeListener(listener)
+    }
+
+    override fun removeOnOffsetChangeListener(listener: OffsetParent.OnOffsetChangeListener) {
+        offsetParentDelegate.removeOnOffsetChangeListener(listener)
+    }
+
+    override fun setVisibility(visibility: Int) {
+        super.setVisibility(visibility)
+    }
+}
--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -1468,6 +1468,7 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
             } else if (prevPage == 0 && mCurrentPage == 1) {
                 navbarAnimator.reverse();
                 getWindowInsetsController().show(WindowInsetsCompat.Type.statusBars());
+                mFirstPagePinnedItem.clearFocus();
             }
         }
     }
