466b3481c5d1f05a907efc610c125454522d4c8e
Yash Garg <yash.garg.ext@murena.com>
2023-03-30 15:28:42 +0530

    feat(search): add swipe down search layout and methods

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -105,7 +105,9 @@ import static com.android.launcher3.util.SettingsCache.TOUCHPAD_NATURAL_SCROLLIN
 
 import android.Manifest;
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.app.Notification;
@@ -151,7 +153,10 @@ import android.view.WindowInsets;
 import android.view.WindowInsetsAnimation;
 import android.view.WindowManager.LayoutParams;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.OvershootInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.window.BackEvent;
 import android.window.OnBackAnimationCallback;
@@ -429,6 +434,8 @@ public class Launcher extends StatefulActivity<LauncherState>
     private LauncherAppMonitor mAppMonitor;
 
     public BlurBackgroundView mBlurLayer;
+    public FrameLayout swipeSearchContainer;
+    private AnimatorSet currentAnimator;
 
     public static Launcher getLauncher(Context context) {
         return fromContext(context);
@@ -606,6 +613,9 @@ public class Launcher extends StatefulActivity<LauncherState>
         mAppMonitor.onLauncherCreated();
         mBlurLayer = findViewById(R.id.blur_layer);
         mBlurLayer.setAlpha(0f);
+
+        swipeSearchContainer = findViewById(R.id.swipe_search_container);
+        swipeSearchContainer.setTranslationY(-1000);
         TraceHelper.INSTANCE.endSection();
 
         getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
@@ -1689,6 +1699,9 @@ public class Launcher extends StatefulActivity<LauncherState>
                 handleSplitAnimationGoingToHome(LAUNCHER_SPLIT_SELECTION_EXIT_HOME);
             }
             mOverlayManager.hideOverlay(isStarted() && !isForceInvisible());
+            if (swipeSearchContainer.getVisibility() == View.VISIBLE) {
+                hideSwipeSearchContainer();
+            }
             handleGestureContract(intent);
         } else if (Intent.ACTION_ALL_APPS.equals(intent.getAction())) {
             showAllAppsFromIntent(alreadyOnHome);
@@ -3219,4 +3232,99 @@ public class Launcher extends StatefulActivity<LauncherState>
     }
 
     // End of Getters and Setters
+
+    public void toggleSwipeSearchState() {
+        if (swipeSearchContainer.getVisibility() == View.VISIBLE) {
+            hideSwipeSearchContainer();
+        } else if (!getWorkspace().isWobbling() && Folder.getOpen(this) == null) {
+            showSwipeSearchContainer();
+        }
+    }
+
+    private void showSwipeSearchContainer() {
+        if (currentAnimator != null) {
+            currentAnimator.cancel();
+        }
+        AnimatorSet set = new AnimatorSet();
+        set.play(ObjectAnimator.ofFloat(swipeSearchContainer, View.TRANSLATION_Y, 0))
+                .with(ObjectAnimator.ofFloat(mBlurLayer, View.ALPHA, 1f))
+                .with(ObjectAnimator.ofFloat(mWorkspace, View.ALPHA, 0f))
+                .with(ObjectAnimator.ofFloat(mWorkspace.mPageIndicator, View.ALPHA, 0f))
+                .with(ObjectAnimator.ofFloat(mHotseat, View.ALPHA, 0f));
+        set.setDuration(300);
+        set.setInterpolator(new LinearInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                swipeSearchContainer.setVisibility(View.VISIBLE);
+                super.onAnimationStart(animation);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                currentAnimator = null;
+                swipeSearchContainer.setVisibility(View.GONE);
+                mWorkspace.setVisibility(View.VISIBLE);
+                mHotseat.setVisibility(View.VISIBLE);
+                mWorkspace.mPageIndicator.setVisibility(View.VISIBLE);
+                mBlurLayer.setAlpha(0f);
+                super.onAnimationCancel(animation);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                currentAnimator = null;
+                mWorkspace.setVisibility(View.GONE);
+                mHotseat.setVisibility(View.GONE);
+                mWorkspace.mPageIndicator.setVisibility(View.GONE);
+                super.onAnimationEnd(animation);
+            }
+        });
+        set.start();
+        currentAnimator = set;
+    }
+
+    public void hideSwipeSearchContainer() {
+        if (currentAnimator != null) {
+            currentAnimator.cancel();
+        }
+        AnimatorSet set = new AnimatorSet();
+        set.play(ObjectAnimator.ofFloat(swipeSearchContainer, View.TRANSLATION_Y, -swipeSearchContainer.getHeight()))
+                .with(ObjectAnimator.ofFloat(mWorkspace, View.ALPHA, 1f))
+                .with(ObjectAnimator.ofFloat(mWorkspace.mPageIndicator, View.ALPHA, 1f))
+                .with(ObjectAnimator.ofFloat(mHotseat, View.ALPHA, 1f))
+                .with(ObjectAnimator.ofFloat(mBlurLayer, View.ALPHA, 0f));
+        set.setDuration(300);
+        set.setInterpolator(new LinearInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mWorkspace.setVisibility(View.VISIBLE);
+                mHotseat.setVisibility(View.VISIBLE);
+                mWorkspace.mPageIndicator.setVisibility(View.VISIBLE);
+                super.onAnimationStart(animation);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                currentAnimator = null;
+                swipeSearchContainer.setVisibility(View.VISIBLE);
+                mBlurLayer.setAlpha(1f);
+                mWorkspace.setVisibility(View.GONE);
+                mHotseat.setVisibility(View.GONE);
+                mWorkspace.mPageIndicator.setVisibility(View.GONE);
+                super.onAnimationCancel(animation);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                currentAnimator = null;
+                swipeSearchContainer.setVisibility(View.GONE);
+                mBlurLayer.setAlpha(0f);
+                super.onAnimationEnd(animation);
+            }
+        });
+        set.start();
+        currentAnimator = set;
+    }
 }
--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -1457,6 +1457,7 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
             } else if (prevPage == 0 && mCurrentPage == 1) {
                 navbarAnimator.reverse();
                 getWindowInsetsController().show(WindowInsetsCompat.Type.statusBars());
+                mFirstPagePinnedItem.clearFocus();
             }
         }
     }
