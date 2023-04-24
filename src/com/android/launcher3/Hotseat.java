15147e02ef902d538b682ff5b847be278e27b46e
Yash Garg <yash.garg.ext@murena.com>
2023-04-24 11:24:15 +0530

    fix: do not draw blur over hotseat if in preview

--- src/com/android/launcher3/Hotseat.java
+++ src/com/android/launcher3/Hotseat.java
@@ -26,7 +26,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -35,7 +34,6 @@ import android.view.ViewDebug;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import com.android.launcher3.folder.Folder;
-import com.android.launcher3.pageindicators.PageIndicatorDots;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
@@ -102,6 +100,7 @@ public class Hotseat extends CellLayout implements Insettable, OffsetParent {
     private final MultiPropertyFactory mIconsTranslationXFactory;
 
     private final View mQsb;
+    public boolean drawBlur;
 
     public Hotseat(Context context) {
         this(context, null);
@@ -115,6 +114,7 @@ public class Hotseat extends CellLayout implements Insettable, OffsetParent {
         super(context, attrs, defStyle);
         mQsb = LayoutInflater.from(context).inflate(R.layout.search_container_hotseat, this, false);
         mBlurDelegate = new BlurViewDelegate(this, BlurWallpaperProvider.Companion.getBlurConfigDock(), null);
+        drawBlur = true;
         setWillNotDraw(false);
         addView(mQsb);
         mIconsAlphaChannels = new MultiValueAlpha(getShortcutsAndWidgets(),
@@ -393,7 +393,7 @@ public class Hotseat extends CellLayout implements Insettable, OffsetParent {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (mBlurDelegate != null) {
+        if (mBlurDelegate != null && drawBlur) {
             mBlurDelegate.draw(canvas);
         }
         super.onDraw(canvas);
