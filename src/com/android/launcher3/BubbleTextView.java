71ba091558476f893fac96214f4835d40c090d4f
Yash Garg <yash.garg.ext@murena.com>
2023-07-21 14:41:52 +0530

    chore(flags): set ENABLE_ICON_LABEL_AUTO_SCALING to false and NOTIFY_CRASHES to true

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -803,6 +803,9 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
     }
 
     private void checkForEllipsis() {
+        if (MultiModeController.isSingleLayerMode()) {
+            return;
+        }
         float width = getWidth() - getCompoundPaddingLeft() - getCompoundPaddingRight();
         if (width <= 0) {
             return;
