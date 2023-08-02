2c029f5cc0209c6463e5b071c479bd19f9547127
TheScarastic <warabhishek@e.email>
2023-08-02 13:45:08 +0530

    fix(multimode): misc fixes for drawer mode

--- src/com/android/launcher3/model/BgDataModel.java
+++ src/com/android/launcher3/model/BgDataModel.java
@@ -17,7 +17,6 @@ package com.android.launcher3.model;
 
 import static android.content.pm.LauncherApps.ShortcutQuery.FLAG_GET_KEY_FIELDS_ONLY;
 
-import static com.android.launcher3.BuildConfig.QSB_ON_FIRST_SCREEN;
 import static com.android.launcher3.BuildConfig.WIDGETS_ENABLED;
 import static com.android.launcher3.Flags.enableSmartspaceRemovalToggle;
 import static com.android.launcher3.LauncherSettings.Favorites.CONTAINER_DESKTOP;
@@ -130,7 +129,7 @@ public class BgDataModel {
      * Load id for which the callbacks were successfully bound
      */
     public int lastLoadId = -1;
-    public boolean isFirstPagePinnedItemEnabled = QSB_ON_FIRST_SCREEN
+    public boolean isFirstPagePinnedItemEnabled = FeatureFlags.QSB_ON_FIRST_SCREEN.get()
             && !enableSmartspaceRemovalToggle();
 
     @Inject
@@ -157,7 +156,7 @@ public class BgDataModel {
                 screenSet.add(item.screenId);
             }
         }
-        if ((FeatureFlags.QSB_ON_FIRST_SCREEN
+        if ((FeatureFlags.QSB_ON_FIRST_SCREEN.get()
                 && !SHOULD_SHOW_FIRST_PAGE_WIDGET)
                 || screenSet.isEmpty()) {
             screenSet.add(Workspace.FIRST_SCREEN_ID);
