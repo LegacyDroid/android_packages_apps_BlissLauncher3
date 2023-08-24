5bf2ad6a1d552b7de83915c594ca17706cbbdddf
TheScarastic <warabhishek@e.email>
2023-08-24 18:36:42 +0530

    fix: folder default naming and click handling

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -3762,7 +3762,10 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
             AtomicInteger index = new AtomicInteger();
 
             mapOverItems((info, view) -> {
-                view.setLayerType(LAYER_TYPE_HARDWARE, null);
+                if (view instanceof BubbleTextView) {
+                    ((BubbleTextView) view).applyUninstallIconState(true);
+                }
+
                 if (excludeDraggingView && mDragObjectInfo != null) {
                     if ((mDragObjectInfo instanceof WorkspaceItemInfo ||
                             mDragObjectInfo instanceof FolderInfo)
@@ -3770,6 +3773,7 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
                         return false;
                     }
                 }
+                view.setLayerType(LAYER_TYPE_HARDWARE, null);
                 index.getAndIncrement();
                 if (view instanceof BubbleTextView || view instanceof FolderIcon) {
                     if (index.get() % 2 == 0) {
@@ -3777,9 +3781,6 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
                     } else {
                         view.startAnimation(getReverseWobbleAnimation());
                     }
-                }
-                if (view instanceof BubbleTextView) {
-                    ((BubbleTextView) view).applyUninstallIconState(true);
                 }
                 return false;
             });
