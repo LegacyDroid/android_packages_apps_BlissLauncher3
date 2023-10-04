2ef149c89464fc6425cff39266c0eb6ec5fd927d
Yash Garg <yash.garg.ext@murena.com>
2023-10-04 12:06:19 +0530

    fix: hide education tips in single layer mode

--- src/com/android/launcher3/widget/picker/WidgetsFullSheet.java
+++ src/com/android/launcher3/widget/picker/WidgetsFullSheet.java
@@ -88,6 +88,7 @@ import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
+import foundation.e.bliss.multimode.MultiModeController;
 import foundation.e.bliss.widgets.WidgetContainer.WidgetFragment;
 
 /**
