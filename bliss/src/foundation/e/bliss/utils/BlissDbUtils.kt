1eac483e7ec57faf6f60cb86047a1a87c47fbe83
althafvly <althafvly@gmail.com>
2023-11-01 11:58:04 +0530

    feat: Fix new weather app migration
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- bliss/src/foundation/e/bliss/utils/BlissDbUtils.kt
+++ bliss/src/foundation/e/bliss/utils/BlissDbUtils.kt
@@ -34,6 +34,7 @@ import com.android.launcher3.LauncherSettings.Favorites.PROFILE_ID
 import com.android.launcher3.model.DatabaseHelper
 import com.android.launcher3.pm.UserCache
 import com.android.launcher3.shortcuts.ShortcutKey
+import foundation.e.bliss.widgets.DefaultWidgets
 import java.net.URISyntaxException
 
 object BlissDbUtils {
@@ -271,13 +272,21 @@ object BlissDbUtils {
 
                         // Get the AppWidgetInfo for the current widget ID
                         val widgetInfo = appWidgetManager.getAppWidgetInfo(id)
+
                         if (widgetInfo != null) {
+                            var provider: ComponentName = widgetInfo.provider
+
+                            // Weather app component name has changed.
+                            if (provider == DefaultWidgets.oldWeatherWidget) {
+                                provider = DefaultWidgets.weatherWidget
+                            }
+
                             widgetsInfoList.add(
                                 WidgetItems(
                                     id,
                                     height,
                                     order,
-                                    widgetInfo.provider,
+                                    provider,
                                 )
                             )
                         }
