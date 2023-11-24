179eaab26bd5a7e7d3025b96eefe80b6ea31ba37
TheScarastic <warabhishek@e.email>
2023-11-24 14:16:16 +0530

    (feat) Hotseat: Auto reorder hotseat icons

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -3318,6 +3318,9 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
     }
 
     private void clearEmptyCell() {
+        // Let Hotseat readjust as well
+        needCellCleanup(getHotseat());
+
         int numberOfScreens = mScreenOrder.size();
         for (int i = 1; i < numberOfScreens; i++) {
             CellLayout cellLayout = mWorkspaceScreens.get(mScreenOrder.get(i));
