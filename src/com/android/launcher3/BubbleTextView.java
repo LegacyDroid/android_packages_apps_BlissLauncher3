19a58f4db06486262c6a46c4d59c25331f15c1c0
TheScarastic <warabhishek@e.email>
2023-12-12 14:10:03 +0530

    feat(notification): Add support for notification dot shadow
    [SahilSonar - adapted to A14]
    [Saalim - adapted to A15]
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -375,6 +375,7 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         mDotInfo = null;
         mDotParams.dotColor = Color.TRANSPARENT;
         mDotParams.appColor = Color.TRANSPARENT;
+        mDotParams.shadowDotColor = Color.TRANSPARENT;
         cancelDotScaleAnim();
         mDotParams.scale = 0f;
         mForceHideDot = false;
@@ -525,6 +526,8 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         mDotParams.appColor = iconDrawable.getIconColor();
         mDotParams.dotColor = getContext().getResources()
                 .getColor(R.color.notification_dot_bg, getContext().getTheme());
+        mDotParams.shadowDotColor = getContext().getResources()
+                .getColor(R.color.notification_dot_shadow, getContext().getTheme());
         setIcon(iconDrawable);
         applyLabel(info);
     }
