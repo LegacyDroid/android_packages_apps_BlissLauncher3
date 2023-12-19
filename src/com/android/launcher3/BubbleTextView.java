45f7eff350a3f4cd3fff47f16aa35aad534bf334
Yash Garg <yash.garg.ext@murena.com>
2023-12-19 16:56:19 +0530

    fix: set bubble text view center vertically
    [SahilSonar - adapted to A14]
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/BubbleTextView.java
+++ src/com/android/launcher3/BubbleTextView.java
@@ -276,7 +276,7 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         mIsRtl = (getResources().getConfiguration().getLayoutDirection()
                 == View.LAYOUT_DIRECTION_RTL);
         mDeviceProfile = mActivity.getDeviceProfile();
-        mCenterVertically = a.getBoolean(R.styleable.BubbleTextView_centerVertically, false);
+        mCenterVertically = a.getBoolean(R.styleable.BubbleTextView_centerVertically, MultiModeController.isSingleLayerMode());
 
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
 
@@ -315,7 +315,6 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
             defaultIconSize = mDeviceProfile.iconSizePx;
             mShouldShowLabel = prefs.getBoolean(KEY_SHOW_DESKTOP_LABELS, true);
         }
-
 
         mIconSize = a.getDimensionPixelSize(R.styleable.BubbleTextView_iconSizeOverride,
                 defaultIconSize);
