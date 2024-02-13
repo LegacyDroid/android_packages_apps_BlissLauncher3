e20116ccf26d329d862c6ecc34814cdd65794ea8
TheScarastic <warabhishek@e.email>
2024-02-13 14:24:28 +0530

    fix(blur) : Add wallpaper scroll to layouts
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -1440,10 +1440,6 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         ((PageIndicatorDots) getPageIndicator()).setForcedTranslationY(dockTranslationY);
 
         mLauncher.mBlurLayer.setAlpha(progress);
-
-        if (getCurrentPage() != 0) {
-            mLauncher.mBlurLayer.setAlpha(0f);
-        }
     }
 
     public void showPageIndicatorAtCurrentScroll() {
