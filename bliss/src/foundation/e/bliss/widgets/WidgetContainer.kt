bd09fa18de26e91dfadb4e9c6b0973ccc208efe4
TheScarastic <warabhishek@e.email>
2024-02-16 15:22:28 +0530

    fix(widget) : Fix configured widget being cancelled yet appearing in removed list

--- bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
+++ bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
@@ -417,7 +417,7 @@ class WidgetContainer(context: Context, attrs: AttributeSet?) :
             if (resultCode == RESULT_OK && requestCode == REQUEST_CONFIGURE_APPWIDGET) {
                 addView(widgetId)
             } else {
-                mWidgetHost.deleteAppWidgetId(widgetId)
+                rebindWidgets()
             }
         }
 
