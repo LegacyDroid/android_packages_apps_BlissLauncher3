fd3a0aac622a18b3b01219d31f6e098782f563c4
TheScarastic <warabhishek@e.email>
2024-02-22 12:57:58 +0530

    fix(pwa): Fix icons not rearranging on pwa removal
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -3400,6 +3400,7 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         if (v instanceof DropTarget) {
             mDragController.removeDropTarget((DropTarget) v);
         }
+        clearEmptyCell();
     }
 
     /**
