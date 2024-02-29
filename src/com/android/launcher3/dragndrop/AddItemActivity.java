b2fe6e1e6745d1532ac9b88e4adc47370ad50f57
TheScarastic <warabhishek@e.email>
2024-02-29 16:44:11 +0530

    fix(pwa): fix app lounge getting unresponsive
    [SahilSonar - adapted to A14]
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/dragndrop/AddItemActivity.java
+++ src/com/android/launcher3/dragndrop/AddItemActivity.java
@@ -156,6 +156,7 @@ public class AddItemActivity extends BaseActivity
                 targetApp = setupShortcut();
                 if (Objects.requireNonNull(mRequest.getShortcutInfo())
                         .getPackage().equals(BlissConstants.APPS_PACKAGE)) {
+                    finish();
                     onPlaceAutomaticallyClick();
                     return;
                 }
