6ebd58eeac271ebb59974dd32bbc8d93f7eb4b9b
TheScarastic <warabhishek@e.email>
2024-03-12 16:58:08 +0530

    feat(widget): Remove shortcuts from widget list
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/widget/picker/WidgetsFullSheet.java
+++ src/com/android/launcher3/widget/picker/WidgetsFullSheet.java
@@ -529,7 +529,14 @@ public class WidgetsFullSheet extends BaseWidgetSheet
             defaultWidgets = dataProvider.getDefaultWidgets();
         } else {
             // This code path can be deleted once enableTieredWidgetsByDefaultInPicker is inlined.
-            widgets = getWidgetsToDisplay();
+            List<WidgetsListBaseEntry> fullWidgets = new ArrayList<>(getWidgetsToDisplay());
+            if (isEditMode) {
+                fullWidgets.removeIf(item -> {
+                    item.mWidgets.removeIf(widget -> widget.widgetInfo == null);
+                    return item.mWidgets.isEmpty();
+                });
+            }
+            widgets = fullWidgets;
         }
 
         AdapterHolder primaryUserAdapterHolder = mAdapters.get(AdapterHolder.PRIMARY);
@@ -739,7 +746,6 @@ public class WidgetsFullSheet extends BaseWidgetSheet
                 getWidgetSheetId(activity),
                 activity.getDragLayer(),
                 false);
-        isEditMode = false;
         sheet.attachToContainer();
         sheet.mIsOpen = true;
         sheet.open(animate);
