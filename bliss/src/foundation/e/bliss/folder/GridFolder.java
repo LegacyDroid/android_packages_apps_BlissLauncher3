7204a566dc31e91cd99ad73f4a84344c8f8018d4
althafvly <althafvly@gmail.com>
2024-04-19 15:36:51 +0530

    feat: Hide folder from recent panel

--- bliss/src/foundation/e/bliss/folder/GridFolder.java
+++ bliss/src/foundation/e/bliss/folder/GridFolder.java
@@ -179,6 +179,7 @@ public class GridFolder extends Folder implements OnAlarmListener {
 
     @Override
     public void onFolderOpenStart() {
+        setVisibility(View.VISIBLE);
         mLastStateBeforeOpen = mLauncher.getStateManager().getState();
         if (!mLauncher.isInState(NORMAL)) {
             mLauncher.getStateManager().goToState(LauncherState.NORMAL, false);
