0ed8c0a756574a136aae977ffdcde47f9c242b48
TheScarastic <warabhishek@e.email>
2024-05-01 14:36:24 +0530

    blisslauncher: Fix abstract crash as seen on sentry
    https://murena-sas.sentry.io/issues/5286234600
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- quickstep/src/com/android/quickstep/LauncherBackAnimationController.java
+++ quickstep/src/com/android/quickstep/LauncherBackAnimationController.java
@@ -52,6 +52,7 @@ import android.window.BackEvent;
 import android.window.BackMotionEvent;
 import android.window.BackProgressAnimator;
 import android.window.IBackAnimationHandoffHandler;
+import android.window.BackProgressAnimator.ProgressCallback;
 import android.window.IOnBackInvokedCallback;
 
 import com.android.app.animation.Animations;
@@ -232,12 +233,16 @@ public class LauncherBackAnimationController {
                 if (controller != null) {
                     controller.initBackMotion(backEvent);
                     controller.tryStartBackAnimation();
-                    mProgressAnimator.onBackStarted(backEvent, event -> {
-                        float backProgress = event.getProgress();
-                        controller.mBackProgress =
-                                mProgressInterpolator.getInterpolation(backProgress);
-                        controller.updateBackProgress(controller.mBackProgress, event);
+                    mProgressAnimator.onBackStarted(backEvent, new ProgressCallback() {
+                        @Override
+                        public void onProgressUpdate(BackEvent event) {
+                            float backProgress = event.getProgress();
+                            controller.mBackProgress =
+                                    mProgressInterpolator.getInterpolation(backProgress);
+                            controller.updateBackProgress(controller.mBackProgress, event);
+                        }
                     });
+
                 }
             });
         }
