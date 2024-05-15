51a187929430f9efd938cabd62cfd52d2713896e
TheScarastic <warabhishek@e.email>
2024-05-15 19:15:02 +0530

    launcher: Dont allow 3rd party apps to place widgets on homescreen
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/CellLayout.java
+++ src/com/android/launcher3/CellLayout.java
@@ -772,7 +772,10 @@ public class CellLayout extends ViewGroup {
     }
 
     public boolean acceptsWidget() {
-        return mContainerType == WORKSPACE;
+        if (MultiModeController.isSingleLayerMode()) {
+            return false;
+        }
+            return mContainerType == WORKSPACE;
     }
 
     /**
