68729e6bf8eac198ddfa81b81b09cdb18426077c
TheScarastic <warabhishek@e.email>
2024-05-15 19:15:02 +0530

    launcher: Dont allow 3rd party apps to place widgets on homescreen
    [SahilSonar - adapted to A14]
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
+++ bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
@@ -454,6 +454,11 @@ class WidgetContainer(context: Context, attrs: AttributeSet?) : FrameLayout(cont
                     defaultWidgets.add(widget.componentName)
                 }
             }
+
+            @JvmStatic
+            fun onWidgetAdded(componentName: ComponentName) {
+                defaultWidgets.add(componentName)
+            }
         }
     }
 }
