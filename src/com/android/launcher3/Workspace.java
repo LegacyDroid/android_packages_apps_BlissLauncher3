e00db44c9cc2d662e15cb0ee552bfa54deea0218
Kshitij Gupta <kshitijgm@gmail.com>
2024-07-09 12:30:25 +0530

    fix: Redo Workspace and CellLayout paddings

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -61,6 +61,7 @@ import android.util.SparseArray;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
@@ -126,6 +127,7 @@ import com.android.launcher3.util.PackageUserKey;
 import com.android.launcher3.util.RunnableList;
 import com.android.launcher3.util.Thunk;
 import com.android.launcher3.util.WallpaperOffsetInterpolator;
+import com.android.launcher3.util.window.WindowManagerProxy;
 import com.android.launcher3.views.FloatingIconView;
 import com.android.launcher3.widget.LauncherAppWidgetHostView;
 import com.android.launcher3.widget.LauncherWidgetHolder;
@@ -374,7 +376,14 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         mWorkspaceFadeInAdjacentScreens = grid.shouldFadeAdjacentWorkspaceScreens();
 
         Rect padding = grid.workspacePadding;
-        setPadding(padding.left, padding.top, padding.right, padding.bottom);
+        // We need padding zeroed out for minus one page.
+        // Also handle vertical bar layout padding manually under
+        // updateCellLayoutPadding
+        if (grid.isLandscape) {
+            setPadding(0, padding.top, 0, padding.bottom);
+        } else {
+            setPadding(padding.left, padding.top, padding.right, padding.bottom);
+        }
         mInsets.set(insets);
 
         if (mWorkspaceFadeInAdjacentScreens) {
@@ -431,18 +440,27 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         mPageIndicator.setLayoutParams(lp);
     }
 
-    private void updateCellLayoutMeasures() {
-        Rect padding = mLauncher.getDeviceProfile().cellLayoutPaddingPx;
-        mWorkspaceScreens.forEach(
-                s -> {
-                    int widgetPadding = getResources().getDimensionPixelSize(R.dimen.widget_page_all_padding);
-                    int paddingTop = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID))? 0 : padding.top;
-                    int paddingBottom = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID))? 0 : padding.bottom;
-                    s.setSpaceBetweenCellLayoutsPx(getPageSpacing() / 4);
-                    int paddingLeft = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID))? widgetPadding : padding.left;
-                    int paddingRight = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID))? widgetPadding : padding.right;
-                    s.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
-                });
+    private void updateCellLayoutPadding() {
+        DeviceProfile grid = mLauncher.getDeviceProfile();
+        Rect padding = grid.cellLayoutPaddingPx;
+        int orientation = WindowManagerProxy.INSTANCE.get(mLauncher).getRotation(mLauncher);
+        int hotseatLeftCorrection = (grid.isVerticalBarLayout() && orientation == Surface.ROTATION_270)
+                ? grid.hotseatBarSizePx : 0;
+        int hotseatRightCorrection = (grid.isVerticalBarLayout() && orientation == Surface.ROTATION_90)
+                ? grid.hotseatBarSizePx : 0;
+
+        mWorkspaceScreens.forEach(s -> {
+            int widgetPadding = getResources().getDimensionPixelSize(R.dimen.widget_page_all_padding);
+            int paddingTop = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID)) ? 0 : padding.top;
+            int paddingBottom = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID)) ? 0 : padding.bottom;
+            int paddingLeft = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID))
+                    ? widgetPadding : (padding.left + hotseatLeftCorrection);
+            int paddingRight = (s == mWorkspaceScreens.get(FIRST_SCREEN_ID))
+                    ? widgetPadding : (padding.right + hotseatRightCorrection);
+
+            s.setSpaceBetweenCellLayoutsPx(getPageSpacing() / 4);
+            s.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+        });
     }
 
     private void updateWorkspaceWidgetsSizes() {
