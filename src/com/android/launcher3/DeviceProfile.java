ba2d2b287e90a5c0603033034d8f9990979223e8
Kshitij Gupta <kshitijgm@gmail.com>
2024-07-30 11:10:21 +0530

    fix: Tune hotseat paddings in no-hint gestural mode
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -1976,9 +1976,11 @@ public class DeviceProfile {
             boolean noHint = isFullyGesture && LineageSettings.System.getInt(
                     context.getContentResolver(), LineageSettings.System.NAVIGATION_BAR_HINT, 0) != 1;
             int hotseatIconMargin = Math.abs(hotseatCellHeightPx - iconSizePx);
+            boolean noHint = isFullyGesture && LineageSettings.System.getInt(
+                    context.getContentResolver(), LineageSettings.System.NAVIGATION_BAR_HINT, 0) != 1;
             // Values obtained by manual validation, independent of dpi and display scale
-            double marginScaleFactor = mInfo.navigationMode == NavigationMode.NO_BUTTON
-                    ? 3.25
+            double marginScaleFactor = isFullyGesture
+                    ? (noHint ? 2.5 : 3.25)
                     : 2.75;
             hotseatBarPadding.set(
                     hotseatAdjustment + workspacePadding.left + cellLayoutPaddingPx.left
@@ -2166,7 +2168,7 @@ public class DeviceProfile {
     }
 
     private int getDeductibleGestureHeight() {
-        if (context == null) return 0;
+        if (isVerticalBarLayout() || context == null) return 0;
         boolean isFullyGesture = mInfo.navigationMode == NavigationMode.NO_BUTTON;
         boolean noHint = isFullyGesture && LineageSettings.System.getInt(
                 context.getContentResolver(), LineageSettings.System.NAVIGATION_BAR_HINT, 0) != 1;
