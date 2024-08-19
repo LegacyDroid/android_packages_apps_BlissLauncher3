7098c78d7b4b94b198267f15dde1cea045b5c555
Kshitij Gupta <kshitijgm@gmail.com>
2024-08-19 17:26:33 +0530

    feat: Re-enable transition and disable centering for non gestural mode
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/LauncherTaskbarUIController.java
+++ quickstep/src/com/android/launcher3/taskbar/LauncherTaskbarUIController.java
@@ -44,6 +44,7 @@ import com.android.launcher3.taskbar.bubbles.BubbleBarController;
 import com.android.launcher3.uioverrides.QuickstepLauncher;
 import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.MultiPropertyFactory;
+import com.android.launcher3.util.NavigationMode;
 import com.android.launcher3.util.OnboardingPrefs;
 import com.android.quickstep.HomeVisibilityState;
 import com.android.quickstep.RecentsAnimationCallbacks;
@@ -438,7 +439,7 @@ public class LauncherTaskbarUIController extends TaskbarUIController {
 
     @Override
     public boolean isHotseatIconOnTopWhenAligned() {
-        return false;
+        return !DisplayController.getNavigationMode(mLauncher).equals(NavigationMode.NO_BUTTON);
     }
 
     @Override
