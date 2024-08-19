860154bef066cc6ff6cc20eb5ea860ace11ff7ee
Kshitij Gupta <kshitijgm@gmail.com>
2024-08-19 11:55:55 +0530

    feat: Improve cellLayout paddings for tablet
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -460,19 +460,22 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         DeviceProfile grid = mLauncher.getDeviceProfile();
         Rect padding = grid.cellLayoutPaddingPx;
         setOrientation(mLauncher);
-        int hotseatLeftCorrection = (grid.isVerticalBarLayout() && mOrientation == Surface.ROTATION_270)
-                ? grid.hotseatBarSizePx : 0;
-        int hotseatRightCorrection = (grid.isVerticalBarLayout() && mOrientation == Surface.ROTATION_90)
-                ? grid.hotseatBarSizePx : 0;
+        int tabletMarginMultiplier = grid.isLandscape ? 10 : 2;
+        int leftCorrection = ((grid.isVerticalBarLayout() && mOrientation == Surface.ROTATION_270)
+                ? grid.hotseatBarSizePx : 0)
+                + (grid.isTablet ? (padding.left * tabletMarginMultiplier) : 0);
+        int rightCorrection = ((grid.isVerticalBarLayout() && mOrientation == Surface.ROTATION_90)
+                ? grid.hotseatBarSizePx : 0)
+                + (grid.isTablet ? (padding.left * tabletMarginMultiplier) : 0);
 
         mWorkspaceScreens.forEach(cellLayout -> {
             int widgetPadding = getResources().getDimensionPixelSize(R.dimen.widget_page_all_padding);
             int paddingTop = (cellLayout == mWorkspaceScreens.get(FIRST_SCREEN_ID)) ? 0 : padding.top;
             int paddingBottom = (cellLayout == mWorkspaceScreens.get(FIRST_SCREEN_ID)) ? 0 : padding.bottom;
             int paddingLeft = (cellLayout == mWorkspaceScreens.get(FIRST_SCREEN_ID))
-                    ? widgetPadding : (padding.left + hotseatLeftCorrection);
+                    ? widgetPadding : (padding.left + leftCorrection);
             int paddingRight = (cellLayout == mWorkspaceScreens.get(FIRST_SCREEN_ID))
-                    ? widgetPadding : (padding.right + hotseatRightCorrection);
+                    ? widgetPadding : (padding.right + rightCorrection);
 
             if (grid.isVerticalBarLayout()) {
                 grid.inv.numRows = grid.inv.numColumnsFixed;
