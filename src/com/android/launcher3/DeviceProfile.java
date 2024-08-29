2b93c72e564c9104e410f9d7def9920f06ca7bbd
Kshitij Gupta <kshitijgm@gmail.com>
2024-08-29 20:27:42 +0530

    fix: Fix UX on rotated phone layouts

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -929,6 +929,12 @@ public class DeviceProfile {
                     + hotseatQsbVisualHeight
                     + hotseatBarBottomSpacePx;
         }
+
+
+        if (mInfo.navigationMode != NavigationMode.NO_BUTTON) {
+            int hotseatIconMargin = Math.abs(hotseatCellHeightPx - iconSizePx);
+            hotseatBarSizePx += (int) (hotseatIconMargin * ICON_OVERLAP_FACTOR);
+        }
     }
 
     /**
