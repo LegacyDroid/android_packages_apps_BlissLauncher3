c5585e3a760c59251e4f00a6721ef11ae19e233b
Kshitij Gupta <kshitijgm@gmail.com>
2024-09-26 18:41:53 +0530

    feat: Animate navigation buttons to center them on home
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarLauncherStateController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarLauncherStateController.java
@@ -805,6 +805,10 @@ public class TaskbarLauncherStateController {
         return mLauncherState.isRecentsViewVisible;
     }
 
+    boolean isInHome() {
+        return mLauncherState == LauncherState.NORMAL;
+    }
+
     private void playStateTransitionAnim(AnimatorSet animatorSet, long duration,
             boolean committed) {
         boolean isInStashedState = mLauncherState.isTaskbarStashed(mLauncher);
