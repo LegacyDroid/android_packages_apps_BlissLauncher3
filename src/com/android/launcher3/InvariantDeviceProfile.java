396bead5c972db4b52db52355acc88c3882e3244
Kshitij Gupta <kshitijgm@gmail.com>
2024-09-26 20:28:29 +0530

    fix: Upscale icons to 1.6x and improve hotseat spacing
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -472,8 +472,8 @@ public class InvariantDeviceProfile implements SafeCloseable, OnSharedPreference
         for (WindowBounds bounds : displayInfo.supportedBounds) {
             boolean isTablet = displayInfo.isTablet(bounds);
             if (isTablet) {
-                iconSize[INDEX_DEFAULT] *= 1.25f;
-                iconSize[INDEX_LANDSCAPE] *= 1.25f;
+                iconSize[INDEX_DEFAULT] *= 1.6f;
+                iconSize[INDEX_LANDSCAPE] *= 1.6f;
                 break;
             }
         }
