5eab76c58b75666603a928ed4c5413d6b0b9e6bf
Kshitij Gupta <kshitijgm@gmail.com>
2024-09-26 17:07:18 +0530

    feat: Disable morph for all
    - Needed for taskbar partial decouple

--- quickstep/src/com/android/launcher3/taskbar/LauncherTaskbarUIController.java
+++ quickstep/src/com/android/launcher3/taskbar/LauncherTaskbarUIController.java
@@ -404,7 +404,7 @@ public class LauncherTaskbarUIController extends TaskbarUIController {
 
     @Override
     public boolean isHotseatIconOnTopWhenAligned() {
-        return !DisplayController.getNavigationMode(mLauncher).equals(NavigationMode.NO_BUTTON);
+        return false;
     }
 
     @Override
