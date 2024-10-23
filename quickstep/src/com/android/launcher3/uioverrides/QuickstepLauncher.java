cedca5ece45890d1962f79db13d48d668da5eebc
althafvly <althafvly@gmail.com>
2024-10-23 13:31:06 +0530

    feat: Don't close folder on wiggle
    [Saalim: Adapted to a15]
    
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -506,7 +506,7 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
 
             // Close any opened folder
             Folder folder = Folder.getOpen(this);
-            if (folder != null && folder.isOpen()) {
+            if (folder != null && folder.isOpen() && !folder.isFolderWobbling()) {
                 folder.close(false);
             }
         }
