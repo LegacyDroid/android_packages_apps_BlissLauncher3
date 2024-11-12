cassidyjames/BlissLauncher3

feat: Properly apply translation for centered dock
- Also manage drag and floating view translation correctly for
  1. Close animation to hotseat
  2. Visibility when dragging icon from hotseat

src/com/android/launcher3/ShortcutAndWidgetContainer.java
@@ -21,6 +21,7 @@ import static android.view.MotionEvent.ACTION_DOWN;
 import static com.android.launcher3.CellLayout.FOLDER;
 import static com.android.launcher3.CellLayout.HOTSEAT;
 import static com.android.launcher3.CellLayout.WORKSPACE;
+import static com.android.launcher3.anim.Interpolators.AGGRESSIVE_EASE_IN_OUT;
 import static com.android.launcher3.util.MultiTranslateDelegate.INDEX_WIDGET_CENTERING;
 
 import android.app.WallpaperManager;
@@ -195,12 +196,42 @@ public class ShortcutAndWidgetContainer extends ViewGroup implements FolderIcon.
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int count = getChildCount();
+        int numOccupied = 0;
         for (int i = 0; i < count; i++) {
             final View child = getChildAt(i);
             if (child.getVisibility() != GONE) {
                 layoutChild(child);
+                numOccupied++;
+                if (child instanceof BubbleTextView) {
+
+                }
             }
         }
+        int translation;
+        if (numOccupied != 0) {
+            final CellLayoutLayoutParams lp = (CellLayoutLayoutParams) getChildAt(0).getLayoutParams();
+            int width = lp.width + mBorderSpace.x;
+            translation = (getWidth() - (numOccupied * width) + mBorderSpace.x) / 2;
+            if (mContainerType == HOTSEAT) {
+                setAnimatedTranslationX(translation);
+            }
+            for (int i = 0; i < count; i++) {
+                final View child = getChildAt(i);
+                if (child.getVisibility() != GONE) {
+                    if (child instanceof BubbleTextView) {
+                        ((BubbleTextView) child).translationX = mContainerType == HOTSEAT ? translation : 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public void setAnimatedTranslationX(float targetX) {
+        this.animate()
+                .translationX(targetX)
+                .setDuration(300)
+                .setInterpolator(AGGRESSIVE_EASE_IN_OUT)
+                .start();
     }
 
     /**

LegacyDroid/android_packages_apps_BlissLauncher3

src/com/android/launcher3/ShortcutAndWidgetContainer.java
@@ -18,6 +18,7 @@ package com.android.launcher3;
 
 import static android.view.MotionEvent.ACTION_DOWN;
 
+import static com.android.app.animation.Interpolators.AGGRESSIVE_EASE_IN_OUT;
 import static com.android.launcher3.CellLayout.FOLDER;
 import static com.android.launcher3.CellLayout.HOTSEAT;
 import static com.android.launcher3.CellLayout.WORKSPACE;
@@ -203,13 +204,44 @@ public class ShortcutAndWidgetContainer extends ViewGroup implements FolderIcon.
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         Trace.beginSection("ShortcutAndWidgetConteiner#onLayout");
         int count = getChildCount();
+        int numOccupied = 0;
         for (int i = 0; i < count; i++) {
             final View child = getChildAt(i);
             if (child.getVisibility() != GONE) {
                 layoutChild(child);
+                numOccupied++;
+                if (child instanceof BubbleTextView) {
+
+                }
             }
         }
         Trace.endSection();
+
+        int translation;
+        if (numOccupied != 0) {
+            final CellLayoutLayoutParams lp = (CellLayoutLayoutParams) getChildAt(0).getLayoutParams();
+            int width = lp.width + mBorderSpace.x;
+            translation = (getWidth() - (numOccupied * width) + mBorderSpace.x) / 2;
+            if (mContainerType == HOTSEAT) {
+                setAnimatedTranslationX(translation);
+            }
+            for (int i = 0; i < count; i++) {
+                final View child = getChildAt(i);
+                if (child.getVisibility() != GONE) {
+                    if (child instanceof BubbleTextView) {
+                        ((BubbleTextView) child).translationX = mContainerType == HOTSEAT ? translation : 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public void setAnimatedTranslationX(float targetX) {
+        this.animate()
+                .translationX(targetX)
+                .setDuration(300)
+                .setInterpolator(AGGRESSIVE_EASE_IN_OUT)
+                .start();
     }
 
     /**