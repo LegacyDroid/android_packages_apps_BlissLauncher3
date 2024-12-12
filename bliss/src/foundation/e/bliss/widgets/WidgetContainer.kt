c12499b52b55254887e080d633506543c6259d75
Kshitij Gupta <kshitijgm@gmail.com>
2024-12-12 14:45:20 +0530

    fix: null-check widgetFragment before cast
    - In case view is not ready, fragment yielded may be null

--- bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
+++ bliss/src/foundation/e/bliss/widgets/WidgetContainer.kt
@@ -142,7 +142,9 @@ class WidgetContainer(context: Context, attrs: AttributeSet?) :
     private fun updateRecyclerViewHeight() {
         mRecyclerView.post {
             val widgetFragment = mLauncher.fragmentManager.findFragmentByTag("qsb_view")
-            (widgetFragment as WidgetFragment).setRecyclerViewHeight()
+            if (widgetFragment != null) {
+                (widgetFragment as WidgetFragment).setRecyclerViewHeight()
+            }
         }
     }
 
