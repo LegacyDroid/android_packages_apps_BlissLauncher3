c529f954dd159a54bb8cce01dffc668daa8056d6
althafvly <althafvly@gmail.com>
2025-01-15 20:22:35 +0530

    feat: align taskbar on orientation change
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarView.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarView.java
@@ -29,6 +29,7 @@ import static com.android.launcher3.icons.IconNormalizer.ICON_VISIBLE_AREA_FACTO
 import static java.util.function.Predicate.not;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Rect;
@@ -146,6 +147,13 @@ public class TaskbarView extends FrameLayout implements FolderIcon.FolderIconPar
     public TaskbarView(@NonNull Context context, @Nullable AttributeSet attrs,
             int defStyleAttr) {
         this(context, attrs, defStyleAttr, 0);
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        onDeviceProfileChanged(mActivityContext.getDeviceProfile());
     }
 
     public TaskbarView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr,
