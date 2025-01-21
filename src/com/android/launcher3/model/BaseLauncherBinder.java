ff5c7067f6bb284c7052edb362b6d0ed06e6f9bc
Kshitij Gupta <kshitijgm@gmail.com>
2025-01-21 16:32:37 +0530

    fix!: Resolve getWriter args in BaseLauncherBinder
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/model/BaseLauncherBinder.java
+++ src/com/android/launcher3/model/BaseLauncherBinder.java
@@ -371,7 +371,7 @@ public class BaseLauncherBinder {
             }
 
             ModelWriter writer = mModel.getWriter(
-                    false /* hasVerticalHotseat */, false /* verifyChanges */, CellPosMapper.DEFAULT, null);
+                    mIDP.getDeviceProfile(mContext).isVerticalBarLayout(), false /* verifyChanges */, CellPosMapper.DEFAULT, null);
             List<Pair<ItemInfo, View>> bindItems = items.stream()
                     .map(i -> Pair.create(i, inflater.inflateItem(i, writer, null)))
                     .collect(Collectors.toList());
