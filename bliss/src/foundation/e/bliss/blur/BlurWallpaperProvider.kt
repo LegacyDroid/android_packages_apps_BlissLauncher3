c752e81d5ff58d815154d462b6d571d767ceeb47
Kshitij Gupta <kshitijgm@gmail.com>
2025-02-24 20:20:43 +0530

    feat: Disable blurring in dock
    - Only use vibrancy in hotseat

--- bliss/src/foundation/e/bliss/blur/BlurWallpaperProvider.kt
+++ bliss/src/foundation/e/bliss/blur/BlurWallpaperProvider.kt
@@ -301,7 +301,7 @@ class BlurWallpaperProvider(val context: Context) {
 
         @JvmField val blurConfigBackground = BlurConfig({ it.background }, 2, 8)
 
-        @JvmField val blurConfigDock = BlurConfig({ it.dock }, 2, 2)
+        @JvmField val blurConfigDock = BlurConfig({ it.dock }, 1, 0)
 
         @JvmField val blurConfigAppGroup = BlurConfig({ it.appGroup }, 6, 8)
 
