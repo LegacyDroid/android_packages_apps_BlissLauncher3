Yash-Garg/BlissLauncher3

feat(badges): show notification count in app badges

src/com/android/launcher3/settings/SettingsActivity.java
@@ -314,6 +314,12 @@ public class SettingsActivity extends FragmentActivity
                     preference.setDefaultValue(isSingleLayer);
                     ((SwitchPreference) preference).setChecked(isSingleLayer);
                     return true;
+
+                case BlissPrefs.PREF_NOTIF_COUNT:
+                    boolean showCount = MultiModeController.isNotifCountEnabled();
+                    preference.setDefaultValue(showCount);
+                    ((SwitchPreference) preference).setChecked(showCount);
+                    return true;
             }
 
             return true;

LegacyDroid/android_packages_apps_BlissLauncher3

src/com/android/launcher3/settings/SettingsActivity.java
@@ -351,6 +351,12 @@ public class SettingsActivity extends FragmentActivity
 
                 case ADD_ICON_PREFERENCE_KEY:
                     return !MultiModeController.isSingleLayerMode();
+
+                case BlissPrefs.PREF_NOTIF_COUNT:
+                    boolean showCount = MultiModeController.isNotifCountEnabled();
+                    preference.setDefaultValue(showCount);
+                    ((SwitchPreferenceCompat) preference).setChecked(showCount);
+                    return true;
             }
 
             return true;