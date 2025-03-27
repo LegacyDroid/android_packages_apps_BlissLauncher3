cassidyjames/BlissLauncher3

feat(widget): Remove shortcuts from widget list

src/com/android/launcher3/widget/picker/WidgetsFullSheet.java
@@ -590,6 +590,13 @@ public class WidgetsFullSheet extends BaseWidgetSheet
         List<WidgetsListBaseEntry> allWidgets =
                 mActivityContext.getPopupDataProvider().getAllWidgets();
 
+        if (isEditMode) {
+            allWidgets.removeIf(item -> {
+                item.mWidgets.removeIf(widget -> widget.widgetInfo == null);
+                return item.mWidgets.isEmpty();
+            });
+        }
+
         AdapterHolder primaryUserAdapterHolder = mAdapters.get(AdapterHolder.PRIMARY);
         primaryUserAdapterHolder.mWidgetsListAdapter.setWidgets(allWidgets);
 
@@ -788,7 +795,6 @@ public class WidgetsFullSheet extends BaseWidgetSheet
                         ? R.layout.widgets_full_sheet_large_screen
                         : R.layout.widgets_full_sheet, launcher.getDragLayer(), false);
 
-        isEditMode = false;
         sheet.attachToContainer();
         sheet.mIsOpen = true;
         sheet.open(animate);

LegacyDroid/android_packages_apps_BlissLauncher3

src/com/android/launcher3/widget/picker/WidgetsFullSheet.java
@@ -487,7 +487,14 @@ public class WidgetsFullSheet extends BaseWidgetSheet
         if (mIsInSearchMode) {
             return;
         }
-        List<WidgetsListBaseEntry> widgets = getWidgetsToDisplay();
+        List<WidgetsListBaseEntry> widgets = new ArrayList<>(getWidgetsToDisplay());
+
+        if (isEditMode) {
+            widgets.removeIf(item -> {
+                item.mWidgets.removeIf(widget -> widget.widgetInfo == null);
+                return item.mWidgets.isEmpty();
+            });
+        }
 
         AdapterHolder primaryUserAdapterHolder = mAdapters.get(AdapterHolder.PRIMARY);
         primaryUserAdapterHolder.mWidgetsListAdapter.setWidgets(widgets);
@@ -696,7 +703,6 @@ public class WidgetsFullSheet extends BaseWidgetSheet
                 getWidgetSheetId(activity),
                 activity.getDragLayer(),
                 false);
-        isEditMode = false;
         sheet.attachToContainer();
         sheet.mIsOpen = true;
         sheet.open(animate);