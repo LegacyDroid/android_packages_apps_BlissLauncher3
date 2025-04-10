dd397a9f1abe5dc2fcc5bce28835178b9bd4f2d5
althafvly <althafvly@gmail.com>
2025-04-10 19:00:42 +0530

    Revert "Trebuchet: Allow changing app suggestion settings"
    This reverts commit 9ef6a1bc5864f0b003dd24a6385f3f20c62520c3.

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -88,9 +88,6 @@ public class SettingsActivity extends FragmentActivity
 
     public static final String KEY_TRUST_APPS = "pref_trust_apps";
 
-    private static final String KEY_SUGGESTIONS = "pref_suggestions";
-    private static final String SUGGESTIONS_PACKAGE = "com.google.android.as";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -356,9 +353,6 @@ public class SettingsActivity extends FragmentActivity
                         return true;
                     });
                     return true;
-
-                case KEY_SUGGESTIONS:
-                    return LineageUtils.isPackageEnabled(getActivity(), SUGGESTIONS_PACKAGE);
             }
             return true;
         }
