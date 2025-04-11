cassidyjames/BlissLauncher3

feat: adjust dot size according to density

src/com/android/launcher3/DeviceProfile.java
@@ -488,8 +488,11 @@ public class DeviceProfile {
 
         // This is done last, after iconSizePx is calculated above.
         Path dotPath = GraphicsUtils.getShapePath(DEFAULT_DOT_SIZE);
-        mDotRendererWorkSpace = new DotRenderer(iconSizePx, dotPath, DEFAULT_DOT_SIZE, showNotificationCount, typeface);
-        mDotRendererAllApps = iconSizePx == allAppsIconSizePx ? mDotRendererWorkSpace :
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int icondotSize = (int) (iconSizePx / (metrics.density / 4));
+        int allAppsIcondotSize = (int) (allAppsIconSizePx / (metrics.density / 4));
+        mDotRendererWorkSpace = new DotRenderer(icondotSize, dotPath, DEFAULT_DOT_SIZE, showNotificationCount, typeface);
+        mDotRendererAllApps = icondotSize == allAppsIcondotSize ? mDotRendererWorkSpace :
                 new DotRenderer(allAppsIconSizePx, dotPath, DEFAULT_DOT_SIZE, showNotificationCount, typeface);
     }
 

LegacyDroid/android_packages_apps_BlissLauncher3

src/com/android/launcher3/DeviceProfile.java
@@ -893,9 +893,11 @@ public class DeviceProfile {
     private static DotRenderer createDotRenderer(
             @NonNull Context context, int size, @NonNull SparseArray<DotRenderer> cache,
             boolean showNotificationCount, Typeface typeface) {
-        DotRenderer renderer = cache.get(size);
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int dotSize = (int) (size / (metrics.density / 4));
+        DotRenderer renderer = cache.get(dotSize);
         if (renderer == null) {
-            renderer = new DotRenderer(size, getShapePath(context, DEFAULT_DOT_SIZE),
+            renderer = new DotRenderer(dotSize, getShapePath(context, DEFAULT_DOT_SIZE),
                     DEFAULT_DOT_SIZE, showNotificationCount, typeface);
             cache.put(size, renderer);
         }