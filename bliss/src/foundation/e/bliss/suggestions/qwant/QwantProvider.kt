bd99283058037991e409ebc912d9e4a415c97a0d
althafvly <althafvly@gmail.com>
2025-04-15 21:08:21 +0530

    feat: Catch http 400 for qwant
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- bliss/src/foundation/e/bliss/suggestions/qwant/QwantProvider.kt
+++ bliss/src/foundation/e/bliss/suggestions/qwant/QwantProvider.kt
@@ -17,10 +17,11 @@
  */
 package foundation.e.bliss.suggestions.qwant
 
-import android.util.Log
 import foundation.e.bliss.suggestions.RetrofitService
 import foundation.e.bliss.suggestions.SuggestionProvider
 import foundation.e.bliss.suggestions.SuggestionsResult
+import retrofit2.HttpException
+import timber.log.Timber
 
 class QwantProvider : SuggestionProvider {
 
@@ -28,13 +29,18 @@ class QwantProvider : SuggestionProvider {
         get() = RetrofitService.getInstance(QwantApi.BASE_URL).create(QwantApi::class.java)
 
     override suspend fun query(query: String): SuggestionsResult {
-        val result = suggestionService.query(query)
-        Log.d("QwantProvider", "Result: $result")
-        return SuggestionsResult(query).apply {
-            networkItems =
-                if (result.status == "success") {
-                    result.data?.items?.map { it.value }?.take(3) ?: emptyList()
-                } else emptyList()
+        return try {
+            val result = suggestionService.query(query)
+            Timber.d("Result: $result")
+            SuggestionsResult(query).apply {
+                networkItems =
+                    if (result.status == "success") {
+                        result.data?.items?.map { it.value }?.take(3) ?: emptyList()
+                    } else emptyList()
+            }
+        } catch (e: HttpException) {
+            Timber.e("HTTP error: ${e.code()} - ${e.message()}")
+            SuggestionsResult(query).apply { networkItems = emptyList() }
         }
     }
 }
