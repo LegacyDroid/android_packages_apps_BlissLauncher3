9c397f416e2c1c1054cd430356a1d93a9bc95dd4
Mohammed Althaf Thayyil <althafvly@gmail.com>
2025-04-17 07:46:39 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- bliss/src/foundation/e/bliss/suggestions/qwant/QwantProvider.kt
+++ bliss/src/foundation/e/bliss/suggestions/qwant/QwantProvider.kt
@@ -33,7 +33,7 @@ class QwantProvider : SuggestionProvider {
             Timber.e("HTTP error: ${e.code()} - ${e.message()}")
             SuggestionsResult(query).apply { networkItems = emptyList() }
         } catch (e: IOException) {
-            Timber.e("HTTP error: $e - ${e.message}")
+            Timber.e("IO error: $e - ${e.message}")
             SuggestionsResult(query).apply { networkItems = emptyList() }
         }
     }
