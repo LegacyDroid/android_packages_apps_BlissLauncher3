Yash-Garg/BlissLauncher3

fix: unhide notification dot on drag close

src/com/android/launcher3/BubbleTextView.java
@@ -65,7 +65,6 @@ import androidx.annotation.UiThread;
 
 import com.android.launcher3.accessibility.LauncherAccessibilityDelegate;
 import com.android.launcher3.dot.DotInfo;
-import com.android.launcher3.dragndrop.DragController;
 import com.android.launcher3.dragndrop.DragOptions;
 import com.android.launcher3.dragndrop.DraggableView;
 import com.android.launcher3.folder.FolderIcon;
@@ -104,7 +103,7 @@ import foundation.e.bliss.wobble.UninstallButtonRenderer;
  * too aggressive.
  */
 public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
-        IconLabelDotView, DraggableView, Reorderable, DragController.DragListener {
+        IconLabelDotView, DraggableView, Reorderable {
 
     private static final int DISPLAY_WORKSPACE = 0;
     private static final int DISPLAY_ALL_APPS = 1;
@@ -1166,7 +1165,9 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         resetIconScale();
         setForceHideDot(true);
         applyUninstallIconState(false);
-        return () -> { };
+        return () -> {
+            setForceHideDot(false);
+        };
     }
 
     private void resetIconScale() {
@@ -1191,14 +1192,5 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         args.put("app_name", appName);
         args.put("count", notificationCount);
         return icuCountFormat.format(args);
-    }
-
-    @Override
-    public void onDragStart(DropTarget.DragObject dragObject, DragOptions options) {
-    }
-
-    @Override
-    public void onDragEnd() {
-        setForceHideDot(false);
     }
 }

LegacyDroid/android_packages_apps_BlissLauncher3

Signed-off-by: Saalim Quadri <danascape@gmail.com>

src/com/android/launcher3/BubbleTextView.java
@@ -74,7 +74,6 @@ import android.view.View;
 import android.view.ViewDebug;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
@@ -84,7 +83,6 @@ import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
 
 import com.android.launcher3.accessibility.BaseAccessibilityDelegate;
 import com.android.launcher3.dot.DotInfo;
-import com.android.launcher3.dragndrop.DragController;
 import com.android.launcher3.dragndrop.DragOptions;
 import com.android.launcher3.dragndrop.DragOptions.PreDragCondition;
 import com.android.launcher3.dragndrop.DraggableView;
@@ -125,7 +123,7 @@ import foundation.e.bliss.wobble.UninstallButtonRenderer;
  * too aggressive.
  */
 public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
-        FloatingIconViewCompanion, DraggableView, Reorderable, DragController.DragListener {
+        FloatingIconViewCompanion, DraggableView, Reorderable {
 
     public static final String TAG = "BubbleTextView";
 
@@ -273,8 +271,6 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         super(context, attrs, defStyle);
         mActivity = ActivityContext.lookupContext(context);
         mLauncher = LauncherAppMonitor.getInstance(context).getLauncher();
-
-        mLauncher.getDragController().addDragListener(this);
 
         FastBitmapDrawable.setFlagHoverEnabled(enableCursorHoverStates());
 
@@ -1564,6 +1560,7 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
         setForceHideDot(true);
         applyUninstallIconState(false);
         return () -> {
+            setForceHideDot(false);
         };
     }
 
@@ -1626,14 +1623,5 @@ public class BubbleTextView extends TextView implements ItemInfoUpdateReceiver,
      */
     public boolean canShowLongPressPopup() {
         return getTag() instanceof ItemInfo && ShortcutUtil.supportsShortcuts((ItemInfo) getTag());
-    }
-
-    @Override
-    public void onDragStart(DropTarget.DragObject dragObject, DragOptions options) {
-    }
-
-    @Override
-    public void onDragEnd() {
-        setForceHideDot(false);
     }
 }