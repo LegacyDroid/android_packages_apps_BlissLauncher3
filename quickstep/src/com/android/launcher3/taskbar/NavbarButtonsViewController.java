4c5f4a0814821d258ba6166a27a750ab9832290d
althafvly <althafvly@gmail.com>
2025-07-16 20:44:14 +0530

    Revert "feat: Disable navbar translation with taskbar"
    This reverts commit 5d762a67ca312556e5eb92be53b9073e66efe454.

--- quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
@@ -132,7 +132,6 @@ public class NavbarButtonsViewController implements TaskbarControllers.LoggableT
 
     private final Rect mTempRect = new Rect();
 
-    private static final boolean NAV_TRANSLATION_DISABLED = true;
     private static final int FLAG_SWITCHER_SHOWING = 1 << 0;
     private static final int FLAG_IME_VISIBLE = 1 << 1;
     private static final int FLAG_ROTATION_BUTTON_VISIBLE = 1 << 2;
@@ -793,7 +792,7 @@ public class NavbarButtonsViewController implements TaskbarControllers.LoggableT
     }
 
     public void updateNavButtonTranslations(boolean force) {
-        if (mContext.isPhoneButtonNavMode() || NAV_TRANSLATION_DISABLED) {
+        if (mContext.isPhoneButtonNavMode()) {
             if (mWindowWidth != 0) {
                 TaskbarUIController uiController = mControllers.uiController;
                 final boolean isInHome = (uiController instanceof LauncherTaskbarUIController
