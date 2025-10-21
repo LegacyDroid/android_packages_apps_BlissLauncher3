583a7967943b68e5c79b4f572b9d10c5a9f673e5
althafvly <althafvly@gmail.com>
2025-10-21 11:35:47 +0530

    Revert "Launcher3: Ability to toggle themed icons for all apps"
    This reverts commit 29609eeb4e32a17ab94faadaa8cab06f3eaa4a02.

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -19,7 +19,6 @@ package com.android.launcher3;
 import static com.android.launcher3.GridType.GRID_TYPE_ANY;
 import static com.android.launcher3.GridType.GRID_TYPE_NON_ONE_GRID;
 import static com.android.launcher3.GridType.GRID_TYPE_ONE_GRID;
-import static com.android.launcher3.LauncherPrefs.ALLAPPS_THEMED_ICONS;
 import static com.android.launcher3.LauncherPrefs.DB_FILE;
 import static com.android.launcher3.LauncherPrefs.ENABLE_TWOLINE_ALLAPPS_TOGGLE;
 import static com.android.launcher3.LauncherPrefs.FIXED_LANDSCAPE_MODE;
@@ -303,8 +302,7 @@ public class InvariantDeviceProfile {
             } else if (ENABLE_TWOLINE_ALLAPPS_TOGGLE.getSharedPrefKey().equals(key)
                     && enableTwoLinesInAllApps != prefs.get(ENABLE_TWOLINE_ALLAPPS_TOGGLE)) {
                 onConfigChanged(context);
-            } else if (ALLAPPS_THEMED_ICONS.getSharedPrefKey().equals(key) ||
-                    SHOW_DESKTOP_LABELS.getSharedPrefKey().equals(key) ||
+            } else if (SHOW_DESKTOP_LABELS.getSharedPrefKey().equals(key) ||
                     SHOW_DRAWER_LABELS.getSharedPrefKey().equals(key)) {
                 onConfigChanged(context);
             }
