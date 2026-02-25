4d0ecdb9b584df4278317a81f1c977305b1d847d
althafvly <althafvly@gmail.com>
2026-02-25 12:35:52 +0530

    Revert "Launcher3: add toggle for desktop and drawer labels"
    This reverts commit d289cf3eda1896a3ab97cfb5c447554e24ce7200.
    
    Change-Id: Idb8a637b835c795b1c9466e08e7bb019fac123e4

--- src/com/android/launcher3/folder/FolderIcon.java
+++ src/com/android/launcher3/folder/FolderIcon.java
@@ -208,9 +208,7 @@ public class FolderIcon extends FrameLayout implements FloatingIconViewCompanion
 
         icon.setClipToPadding(false);
         icon.mFolderName = icon.findViewById(R.id.folder_icon_name);
-        if (icon.mFolderName.shouldShowLabel()) {
-            icon.mFolderName.applyLabel(folderInfo.title);
-        }
+        icon.mFolderName.applyLabel(folderInfo.title);
         icon.mFolderName.setCompoundDrawablePadding(0);
         FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) icon.mFolderName.getLayoutParams();
         lp.topMargin = grid.getWorkspaceIconProfile().getIconSizePx()
