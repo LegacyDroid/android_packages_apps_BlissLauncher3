8c32b36d990d45545b763fcc2eef90b2a389f0f1
althafvly <althafvly@gmail.com>
2026-05-11 11:39:15 +0530

    feat: Fix widget restore after backup

--- src/com/android/launcher3/LauncherBackupAgent.java
+++ src/com/android/launcher3/LauncherBackupAgent.java
@@ -1,5 +1,7 @@
 package com.android.launcher3;
 
+import static com.android.launcher3.LauncherPrefs.NEEDS_WIDGET_REBIND_AFTER_RESTORE;
+
 import android.app.backup.BackupAgent;
 import android.app.backup.BackupDataInput;
 import android.app.backup.BackupDataOutput;
@@ -48,5 +50,6 @@ public class LauncherBackupAgent extends BackupAgent {
     @Override
     public void onRestoreFinished() {
         RestoreDbTask.setPending(this);
+        LauncherPrefs.get(this).putSync(NEEDS_WIDGET_REBIND_AFTER_RESTORE.to(true));
     }
 }
